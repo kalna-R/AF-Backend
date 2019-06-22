b0a74871b6d1e3cc589b8479a0e427e4eabdcb53
Janith Silva <janithravindu@gmail.com>
2019-06-22 12:46:59 +0530

    chages to router

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 4000;
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const routes = express.Router();
 
 //classes which handles routes
-var adminRouter = require('./admin/admin.controller');
-var instructorRouter = require('./instructor/instructor.controller');
-var courseRouter = require('./course/courseController');
+var adminRouter = require("./admin/admin.controller");
+var instructorRouter = require("./instructor/instructor.controller");
+var courseRouter = require("./course/courseController");
+var examRouter = require("./routes/assignment.exam.routes");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //open up a connection to the database
-mongoose.connect('mongodb://localhost/infosysdb',{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost/infosysdb", { useNewUrlParser: true });
 
 var connection = mongoose.connection;
-connection.once('open', function(){
-    console.log("Connection Established");
-}); 
+connection.once("open", function() {
+  console.log("Connection Established");
+});
 
 //route the requests to the specified class
-routes.use('/admin', adminRouter);
-routes.use('/instructor', instructorRouter);
-routes.use('/course', courseRouter);
+routes.use("/admin", adminRouter);
+routes.use("/instructor", instructorRouter);
+routes.use("/course", courseRouter);
+routes.use("/exam", examRouter);
 
-app.use('/', routes);
+app.use("/", routes);
 
-app.listen(PORT, function(){
-    console.log("Server running on port: "+PORT);
-})
+app.listen(PORT, function() {
+  console.log("Server running on port: " + PORT);
+});
